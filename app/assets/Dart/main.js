8e154fab1eb8f8cad2320ba95d17ee15dfc2024d
Anastasiia <anastasiia0905@gmail.com>
2018-09-10 23:39:11 +0300

    better semantic and organize css

---
+++ app/assets/Dart/main.js
@@ -0,0 +1,10 @@
+window.onscroll = function(){stickyHeader()};
+function stickyHeader(){
+    let header = document.querySelector("header");
+    let stycky = header.offsetTop;
+    if(window.pageYOffset > stycky){
+        header.classList.add('sticky');
+    } else {
+        header.classList.remove('sticky');
+    }
+}
