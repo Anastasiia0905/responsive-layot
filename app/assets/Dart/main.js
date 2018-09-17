702464bd730b09fb9ad8b90b697d2fdec15df4f4
Anastasiia <anastasiia0905@gmail.com>
2018-09-17 20:25:27 +0300

    add sticky nav

--- app/assets/Dart/main.js
+++ app/assets/Dart/main.js
@@ -1,6 +1,6 @@
 window.onscroll = function(){stickyHeader()};
 function stickyHeader(){
-    let header = document.querySelector("header");
+    let header = document.querySelector(".header");
     let stycky = header.offsetTop;
     if(window.pageYOffset > stycky){
         header.classList.add('sticky');
